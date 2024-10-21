fa817f75d5eb32fe8e25b2de42f0205757464e42
ban4ello <ban4ellog@gmail.com>
2024-10-21 06:23:27 +0300

    changed hostname for db

--- db.js
+++ db.js
@@ -2,9 +2,11 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
 	user: 'postgres',
 	password: 'root',
-	host: 'localhost',
+	host: 'https://90.156.171.85',
+	// host: 'localhost',
 	port: 5432,
-	database: 'calculations-analysis',
+	database: 'postrges',
+	// database: 'calculations-analysis',
 	// database: 'node_postrges',
 })
 
