b946e97f4dd085c9ac90eb12fc6b67ee661de203
ban4ello <ban4ellog@gmail.com>
2025-01-13 11:59:38 +0300

    fixed db.js

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-	user: 'calculations', // postgres
+	// user: 'postgres',
+	// database: 'postgres',
+	user: 'calculations',
+	database: 'calculations',
 	password: 'root',
 	host: 'localhost',
 	port: 5432,
-	database: 'calculations', // postgres
 })
 
 module.exports = pool
